7a1187448e0aea89da01fe8898b1b829a7960d25
Anthony Moe <anthony.moe7@gmail.com>
2021-04-01 14:50:01 -0500

    get and post routes working for pizza and order

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,8 +1,57 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import './App.css';
 
+
 function App() {
+  const [newOrder, setNewOrder] = useState([]);
+
+  useEffect(() => {
+    console.log('in useEffect')
+    getMenu();
+    getOrder();
+  }, []);
+
+//This function returns the data from the pizza table in the db. basically the menu
+//it will then be rendered to the dom below using map
+  const getMenu = () => {
+    axios({
+      method: 'GET',
+      url: '/api/pizza'
+    }).then( (response) => {
+        console.log( 'back from getPizza with:', response );
+    }).catch( (err) => { 
+        console.log(err);
+        alert( 'problem in getMenu' )
+    })
+  }//end of getMenu
+
+  //Will add to customer order 
+  //tested it by pluggin in object and logging post route. WORKING
+  const addOrder = () => {
+    axios({
+      method: 'POST',
+      url: '/api/order',
+      data: newOrder
+    }).then ( ( response ) => {
+      console.log('back from POST with:', response);
+    }).catch( (err) => {
+      console.log( err );
+      alert( 'err in addOrder' );
+    })
+  }//end of addOrder
+
+  const getOrder = () => {
+    axios({
+      method: 'GET',
+      url: '/api/order'
+    }).then( (response) => {
+        console.log( 'back from getOrder with:', response );
+    }).catch( (err) => { 
+        console.log(err);
+        alert( 'problem in getOrder' )
+    })
+  }//end of getOrder
 
   return (
     <div className='App'>
